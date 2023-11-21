1e587f6a71d0c180523d7c472a5ad863aff6d6db
kysrit <102626439+kysrit@users.noreply.github.com>
2023-11-21 16:50:05 -0500

    Add files via upload

---
+++ js/bigimgpets.js
@@ -0,0 +1,63 @@
+function myFunction(imgs) {
+    var x = 600;
+    w = window.innerWidth;
+    if(x<w){
+    var bigimg = document.getElementById("largeImg");
+    bigimg.src = imgs.src;
+    bigimg.parentElement.style.display = "block";
+    }
+  }
+  function myFunction1(imgs) {
+    var x = 600;
+    w = window.innerWidth;
+    if(x<w){
+    var bigimg = document.getElementById("largeImg1");
+    bigimg.src = imgs.src;
+    bigimg.parentElement.style.display = "block";
+    }
+  }
+  function myFunction2(imgs) {
+    var x = 600;
+    w = window.innerWidth;
+    if(x<w){
+    var bigimg = document.getElementById("largeImg2");
+    bigimg.src = imgs.src;
+    bigimg.parentElement.style.display = "block";
+    }
+  }
+  function myFunction3(imgs) {
+    var x = 600;
+    w = window.innerWidth;
+    if(x<w){
+    var bigimg = document.getElementById("largeImg3");
+    bigimg.src = imgs.src;
+    bigimg.parentElement.style.display = "block";
+    }
+  }
+  function myFunction4(imgs) {
+    var x = 600;
+    w = window.innerWidth;
+    if(x<w){
+    var bigimg = document.getElementById("largeImg4");
+    bigimg.src = imgs.src;
+    bigimg.parentElement.style.display = "block";
+    }
+  }
+  function myFunction5(imgs) {
+    var x = 600;
+    w = window.innerWidth;
+    if(x<w){
+    var bigimg = document.getElementById("largeImg5");
+    bigimg.src = imgs.src;
+    bigimg.parentElement.style.display = "block";
+    }
+  }
+  function myFunction6(imgs) {
+    var x = 600;
+    w = window.innerWidth;
+    if(x<w){
+    var bigimg = document.getElementById("largeImg6");
+    bigimg.src = imgs.src;
+    bigimg.parentElement.style.display = "block";
+    }
+  }
---
+++ js/bigimgrecipe.js
@@ -0,0 +1,27 @@
+function myFunction(imgs) {
+    var x = 600;
+    w = window.innerWidth;
+    if(x<w){
+    var bigimg = document.getElementById("largeImg");
+    bigimg.src = imgs.src;
+    bigimg.parentElement.style.display = "block";
+    }
+  }
+  function myFunction1(imgs) {
+    var x = 600;
+    w = window.innerWidth;
+    if(x<w){
+    var bigimg = document.getElementById("largeImg1");
+    bigimg.src = imgs.src;
+    bigimg.parentElement.style.display = "block";
+    }
+  }
+  function myFunction2(imgs) {
+    var x = 600;
+    w = window.innerWidth;
+    if(x<w){
+    var bigimg = document.getElementById("largeImg2");
+    bigimg.src = imgs.src;
+    bigimg.parentElement.style.display = "block";
+    }
+  }
